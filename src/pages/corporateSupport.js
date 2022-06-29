5fdb3b89107a2dac64377835e74272a5fcfaf940
Harry Xu <harryxu2626@gmail.com>
2022-06-28 21:19:34 -0500

    added updated sponsor packet/logo

--- src/pages/corporateSupport.js
+++ src/pages/corporateSupport.js
@@ -5,8 +5,9 @@ import BtmAppBar from "../components/btmAppBar";
 import Typography from "@mui/material/Typography";
 import { StaticImage } from "gatsby-plugin-image";
 import useDarkMode from "use-dark-mode";
-import { isMobile } from "react-device-detect"
-import "../styles/index.css"
+import { isMobile } from "react-device-detect";
+import sponsorPacket from "../files/Duke_AERO_Sponsor_Packet.pdf";
+import "../styles/index.css";
 
 const pageStyles = {
   paddingLeft: "10vw",
@@ -15,13 +16,13 @@ const pageStyles = {
 };
 
 const CorporateSupport = () => {
-  const [sysPref, setSysPref] = React.useState(false)
-  React.useEffect(()=>{
-    setSysPref(window.matchMedia('(prefers-color-scheme: dark)').matches)
-  },[])
-    const darkMode = useDarkMode(sysPref);
+  const [sysPref, setSysPref] = React.useState(false);
+  React.useEffect(() => {
+    setSysPref(window.matchMedia("(prefers-color-scheme: dark)").matches);
+  }, []);
+  const darkMode = useDarkMode(sysPref);
 
-    return (
+  return (
     <div style={pageStyles}>
       <SEO title="Corporate Support" />
       <TopAppBar />
@@ -56,7 +57,7 @@ const CorporateSupport = () => {
       </Typography>
 
       <Typography
-        style={{ marginTop: "3vh" }}
+        style={{ marginTop: "2vh" }}
         sx={{ typography: { xs: "body1", md: "h6" } }}
         textAlign="center"
       >
@@ -67,29 +68,35 @@ const CorporateSupport = () => {
         style={{
           display: darkMode.value ? "none" : "flex",
           justifyContent: "center",
-          marginTop: "5vh",
+          marginTop: "0.5vh",
         }}
       >
         <figure style={{ margin: 0 }}>
-          <StaticImage
-            style={{ margin: "2vw" }}
-            width={275}
-            height={275}
-            placeholder="blurred"
-            src="../images/handbook.png"
-            alt="corporate handbook"
-          />
+          <a href={sponsorPacket} target="_blank" rel="noopener noreferrer">
+            <StaticImage
+              style={{ margin: "2vw" }}
+              width={275}
+              height={350}
+              placeholder="blurred"
+              src="../images/duke_aero_sponsorship_cover.png"
+              alt="corporate handbook"
+            />
+          </a>
           <figcaption style={{ textAlign: "center" }}>
-            Link to Outreach Packet - COMING SOON
+            Outreach Packet
           </figcaption>
         </figure>
 
         <figure style={{ margin: 0 }}>
-          <a href="https://forms.gle/rKSAkqfxndfB1NQY8" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://forms.gle/rKSAkqfxndfB1NQY8"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <StaticImage
               style={{ margin: "2vw" }}
               width={275}
-              height={275}
+              height={350}
               placeholder="blurred"
               src="../images/aero_logo.png"
               alt="aero logo blue"
@@ -105,29 +112,35 @@ const CorporateSupport = () => {
         style={{
           display: darkMode.value ? "flex" : "none",
           justifyContent: "center",
-          marginTop: "5vh",
+          marginTop: "0.5vh",
         }}
       >
         <figure style={{ margin: 0 }}>
-          <StaticImage
-            style={{ margin: "2vw" }}
-            width={275}
-            height={275}
-            placeholder="blurred"
-            src="../images/handbook.png"
-            alt="corporate handbook"
-          />
+          <a href={sponsorPacket} target="_blank" rel="noopener noreferrer">
+            <StaticImage
+              style={{ margin: "2vw" }}
+              width={275}
+              height={350}
+              placeholder="blurred"
+              src="../images/duke_aero_sponsorship_cover.png"
+              alt="corporate handbook"
+            />
+          </a>
           <figcaption style={{ textAlign: "center" }}>
-            Link to Outreach Packet - COMING SOON
+            Sponsor Packet
           </figcaption>
         </figure>
 
         <figure style={{ margin: 0 }}>
-          <a href="https://forms.gle/rKSAkqfxndfB1NQY8" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://forms.gle/rKSAkqfxndfB1NQY8"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <StaticImage
               style={{ margin: "2vw" }}
               width={275}
-              height={275}
+              height={350}
               placeholder="blurred"
               src="../images/aero_logo_square_white.png"
               alt="aero logo white"
@@ -139,7 +152,7 @@ const CorporateSupport = () => {
         </figure>
       </div>
 
-      <BtmAppBar forceLoad={isMobile ? false : true}/>
+      <BtmAppBar forceLoad={isMobile ? false : true} />
     </div>
   );
 };
